eb60bfd0d7f4758a5d0d366003382258b0dc0821
ABODJI-Kondi-Kaled <kondi.kaled.abodji@gmail.com>
2025-08-05 18:04:30 +0100

    developping accounts application | custom User | Admin setting up

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,108 @@
 from django.contrib import admin
+from django import forms
 
-# Register your models here.
+# from django.contrib.auth.forms import UserCreationForm as BaseUserCreationForm
+# from django.contrib.auth.forms import UserChangeForm as BaseUserChangeForm
+
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
+from django.contrib.auth.forms import ReadOnlyPasswordHashField
+
+# Traduction
+from django.utils.translation import gettext_lazy as _
+
+from .models import User
+
+# Creating a custom admin Form
+
+class UserCreationForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['email', 'password1', 'password2']
+    
+    password1 = forms.CharField(label=_('Password'), widget=forms.PasswordInput)
+    password2 = forms.CharField(label=_('Confirm Password'), widget=forms.PasswordInput)
+
+    def clean_password2(self):
+        pass1 = self.cleaned_data.get('password1')
+        pass2 = self.cleaned_data.get('password2')
+
+        if pass1 and pass2 and pass1 != pass2:
+            raise ValueError(_('Passwords don\'t match'))
+        
+        return pass2
+    
+    def save(self, commit=True):
+        """
+        Args:
+            - commit : Bool that defines if the intance of the model user should be save in db or not
+        
+        """
+        user = super().save(commit=False)
+
+        user.set_password(self.cleaned_data.get('password2'))
+
+        if commit:
+            user.save()
+        return user
+    
+class UserChangeForm(forms.ModelForm):
+    password = ReadOnlyPasswordHashField()
+
+    class Meta:
+        model = User
+        fields = '__all__'
+
+
+class UserAdmin(BaseUserAdmin):
+    form = UserChangeForm
+    add_form = UserCreationForm
+
+    # fields to be added in the Admin panel
+
+    list_display = [
+        'email',
+        'first_name',
+        'last_name',
+        'is_active',
+        'is_staff',
+        'is_superuser',
+
+    ]
+
+    list_filter = ['is_active', 'is_staff', 'groups']
+    search_fields = ['email', 'first_name', 'last_name']
+    ordering = ['email']
+    
+    fieldsets =(
+        (None, {'fields':('email', 'password')}),
+        (_('Personal Informations'), {
+            'fields': ('last_name', 'first_name')
+        }),
+        (_('Permissions'), {
+            'fields':('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')
+        }),
+
+        (_('Important Dates'), {
+            'fields':('last_login',)
+        }),
+    )
+
+    add_fieldsets = (
+        (
+            None,
+            {
+                'classes':('wide',),
+                'fields':[
+                    'email', 'last_name', 'first_name',
+                    'is_active', 'is_staff', 'is_superuser', 
+                    'groups', 'user_permissions',           
+                ]
+            },
+        ),
+    )
+    
+admin.site.register(User, UserAdmin)
+    
+
+
--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,82 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+# Creating Custom user model
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, PermissionsMixin
+
+
+
+# Custom user Manager
+class UserManager(BaseUserManager):
+    """
+    In charge of managing the custom user model
+    """
+    def create_user(self, email, password=None, **extra_fields):
+        """
+        Args:
+            - email : user login email
+            - password : user password
+            - **extra_fields: dictionnary containing extrafields ('is_staff', 'is_active'..)
+        Returns:
+            created user
+        """
+        if not email:
+            raise ValueError(_('Email is compulsory'))
+        
+        email = self.normalize_email(email)
+        user = self.model(email = email, **extra_fields)
+
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+    
+    def create_superuser(self, email, password=None, **extra_fields):
+        """
+        Args:
+            - extra_fields: the additionnal fields ('is_superuser'..)
+        Returns :
+            - Created user
+        """
+        
+        extra_fields.setdefault('is_active', True)
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if not extra_fields.get('is_staff'):
+            raise ValueError(_('\'is_staff\' must be = True'))
+
+        if not extra_fields.get('is_superuser'):
+            raise ValueError(_('\'is_superuser\' must be = True'))
+        
+        
+        return self.create_user(email, password, **extra_fields)
+    
+class User(AbstractBaseUser, PermissionsMixin):
+    """
+    The custom user model.
+    - Defines email as USERNAME_FIELD
+    """
+
+    email = models.EmailField(_('Email'), unique=True)
+    last_name = models.CharField(_('Last Name'), max_length=255, blank=True)
+    first_name = models.CharField(_('First name'), max_length=255, blank=True)
+
+    is_active = models.BooleanField(_('Account activated'), default=True, help_text=_("" \
+    "Defines wether the user's accounts can be use or not (a temporal delection control)"))
+
+    is_staff = models.BooleanField(_('Member of the staff'), default=False)
+    
+    REQUIRED_FIELDS=[]
+    USERNAME_FIELD='email'
+
+    objects = UserManager()
+
+    class Meta:
+        verbose_name = _('user')
+        verbose_name_plural = _('users')
+
+    def __str__(self):
+        return self.email
+
+    
